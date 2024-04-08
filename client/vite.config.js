ed0657619ddb94ad439526d021ae4310289682c7
IgnacioBinaghi <binaghiignacio@gmail.com>
2024-04-08 12:40:27 -0400

    fixes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://notie.onrender.com/'
+      '/api': {
+        target: 'https://notie.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      }
     }
   }
-})
+});
